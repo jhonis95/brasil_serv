cffbd3fdf6c8826809b794a392250c29f5a3b242
Jonatan <j.jhonis95@gmail.com>
2025-09-25 11:11:26 -0300

    adding all links

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,12 @@ interface Window {
   Alpine: import('alpinejs').Alpine;
   animate: typeof import('motion').animate; 
 }
+
+interface ImportMetaEnv {
+  readonly WHATSAPP_NUMBER: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
